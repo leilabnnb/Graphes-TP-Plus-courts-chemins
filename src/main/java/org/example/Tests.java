52ea5b724f8afab88d890d7a75bf9ece3bacb5a1
bs204037 <safa-leila.bennabi@etu.univ-lehavre.fr>
2023-11-29 04:38:03 +0100

    tests sur graphes différents

--- src/main/java/org/example/Tests.java
+++ src/main/java/org/example/Tests.java
@@ -19,10 +19,10 @@ public class Tests {
 
             // 0 < density <= 1
             double density = 0.1;
-            int avgDegree = 600;
+            int avgDegree = 500;
             int maxWeight = 10;
 
-            int numTests = 5;
+            int numTests = 10;
 
 
 
@@ -36,24 +36,27 @@ public class Tests {
             int [] order = {1000, 2000, 3000, 4000,5000,6000,7000, 8000, 9000, 10000};
             //int [] order = {20000, 40000, 60000, 80000, 100000};
 
-            String suffixToFileName1 = "_S_density"+ density;
-            String suffixToFileName2 = "_S_degree"+ avgDegree;
+            String suffixToFileName1 = "_M_density"+ density;
+            String suffixToFileName2 = "_L_degree"+ avgDegree;
 
-            String path1 = System.getProperty(("user.dir")) + File.separator + "dataFiles/myDijkstra" + suffixToFileName1+".dat";
-            String path2 = System.getProperty(("user.dir")) + File.separator + "dataFiles/theDijkstra" + suffixToFileName1+".dat";
+            String path1 = System.getProperty(("user.dir")) + File.separator + "dataFiles/myDijkstra" + suffixToFileName2+".dat";
+            String path2 = System.getProperty(("user.dir")) + File.separator + "dataFiles/theDijkstra" + suffixToFileName2+".dat";
             FileWriter myDijkstraFile = new FileWriter(path1);
             FileWriter theDijkstraFile = new FileWriter(path2);
 
             System.out.println("m1");
 
             for(int o : order) {
+
+                double totalDurationMyDijkstra = 0;
+                double totalDurationTheDijkstra = 0;
                 // generate graph and choose source node
-                //Graph g1 = generateDensity(o, density, maxWeight, false);
+                //Graph g1 = generateDensity(o, density, maxWeight, true);
                 Graph g1 = generate(o, avgDegree, maxWeight, true);
                 Node s = g1.getNode(0);
                 System.out.println("m2");
 
-                //for (int i = 0; i < numTests; i++) {
+                for (int i = 0; i < numTests; i++) {
 
                     System.out.println("m3");
 
@@ -65,9 +68,9 @@ public class Tests {
 
                     /// duration in miliseconds
                     double durationMyDijkstra = (end - start);
-                    myDijkstraFile.write(o + " " + durationMyDijkstra + "\n");
+                    //myDijkstraFile.write(o + " " + durationMyDijkstra + "\n");
                     // End test MyDijkstra algo
-
+                    totalDurationMyDijkstra += durationMyDijkstra;
                     System.out.println("m4");
 
                     // Test GS's algo
@@ -83,13 +86,17 @@ public class Tests {
 
                     /// duration in miliseconds
                     double durationTheDijkstra = (end - start);
-                    System.out.println("m6");
+                    totalDurationTheDijkstra += durationTheDijkstra;
 
-                    theDijkstraFile.write(o + " " + durationTheDijkstra + "\n");
 
                     // End test GS's algo
                 }
-            //}
+
+                double avgDurationMyDijkstra = totalDurationMyDijkstra / numTests;
+                double avgDurationTheDijkstra = totalDurationTheDijkstra / numTests;
+                myDijkstraFile.write(o + " " + avgDurationMyDijkstra + "\n");
+                theDijkstraFile.write(o + " " + avgDurationTheDijkstra + "\n");
+            }
             System.out.println("m7");
 
             myDijkstraFile.close();
